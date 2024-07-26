c4654bea9e7413b8361dd2fa331630c7c38d9ba2
Luisa-rb <164709877+Luisa-rb@users.noreply.github.com>
2024-07-26 10:01:52 -0300

    Update Distancia_entre_amigos_beecrowd_3050.c
    second commit

--- Distancia_entre_amigos_beecrowd_3050.c
+++ Distancia_entre_amigos_beecrowd_3050.c
@@ -7,19 +7,13 @@ int main(){
     int n, andares[max], dis, maior; 
     
     scanf("%d", &n);
-
     scanf("%d", &andares[0]);
-
-    // andares[0]--;
     
     maior = 0;
 
     for(int i = 1; i < n; i++){
 
         scanf("%d", &andares[i]);
-        // andares[i]--;
-
-        // dis = (andares [i - 1] - 1) + (andares[i] - 1) + (i - (i - 1));
 
         for (int j=i-1; j >= 0; j--) {
 
